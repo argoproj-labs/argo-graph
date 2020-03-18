e376d5249b961e1a0b105994ae84ab9edc022c13
Alex Collins <alex_collins@intuit.com>
2020-03-18 12:32:25 -0700

    attributes

--- ui/src/app/node-info-panel.tsx
+++ ui/src/app/node-info-panel.tsx
@@ -23,7 +23,27 @@ export class NodeInfoPanel extends React.Component<{ guid: string }, State> {
     public render() {
         return (
             <div>
-                <h4>{this.state.node && this.state.node.label}</h4>
+                {this.state.node && (<>
+                    <h4>{this.state.node.label}</h4>
+                    <div className='white-box'>
+                        <div className='row'>
+                            <div className='columns small-3'>CLUSTER</div>
+                            <div className='columns small-9'>{Node.getCluster(this.state.node)}</div>
+                        </div>
+                        <div className='row'>
+                            <div className='columns small-3'>NAMESPACE</div>
+                            <div className='columns small-9'>{Node.getNamespace(this.state.node)}</div>
+                        </div>
+                        <div className='row'>
+                            <div className='columns small-3'>KIND</div>
+                            <div className='columns small-9'>{Node.getKind(this.state.node)}</div>
+                        </div>
+                        <div className='row'>
+                            <div className='columns small-3'>NAME</div>
+                            <div className='columns small-9'>{Node.getName(this.state.node)}</div>
+                        </div>
+                    </div>
+                </>)}
             </div>
         );
     }
